82e1ee65aaf86ed0056e8cc1265284c43326bdc6
ehcaw <ryannguyenc@gmail.com>
2024-06-23 12:44:18 -0700

    added api route

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8329d2bac29eef52"; }
+/******/ 		__webpack_require__.h = function() { return "15a6a1dda3e5725a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/api/openai.ts
+++ src/pages/api/openai.ts
@@ -5,6 +5,37 @@ const openaikey = process.env.OPEN_AI_KEY;
 
 const openai = new OpenAI({ apiKey: openaikey });
 
+function csvToJson(csvString: string) {
+  const rows = csvString.split("\n");
+
+  const headers = rows[0].split(",");
+
+  const jsonData = [];
+  for (let i = 1; i < rows.length; i++) {
+    const values = rows[i].split(",");
+
+    const obj: { [key: string]: string } = {};
+
+    for (let j = 0; j < headers.length; j++) {
+      const key = headers[j].trim();
+      const value = values[j].trim();
+
+      obj[key] = value;
+    }
+
+    jsonData.push(obj);
+  }
+  return JSON.stringify(jsonData);
+}
+
+async function formatCsvForPayload(csvString: string) {
+  let formattedCsv = csvString.replace(/"/g, '\\"');
+  // Then, replace newlines with \n to format the entire CSV as a single line string
+  formattedCsv = formattedCsv.replace(/\n/g, "\\n");
+  // Return the formatted CSV string
+  return formattedCsv;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -12,14 +43,16 @@ export default async function handler(
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
-  const file = JSON.stringify(req.body.data);
+  const formattedCsv = await formatCsvForPayload(req.body.file);
+  console.log(formattedCsv);
+  const file = csvToJson(formattedCsv);
   const mode = req.body.mode;
   const response = await openai.chat.completions.create({
     messages: [
       {
         role: "system",
         content:
-          "you are a product analyzer assistant meant to predict based on the mode, use the data in the file to predict what the user will need",
+          "you are a product analyzer assistant meant to predict based on the mode, use the data in the file to predict what the user will need in a json format",
       },
       {
         role: "assistant",
